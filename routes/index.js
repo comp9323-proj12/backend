169357c6452edc1e395a63fcddb5287733ed4f2b
feifeirun <feifeirun97@gmail.com>
2021-07-21 00:23:43 +1000

    fetch

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
-const router = require('koa-router')();
-const article = require('./article');
-
+const router = require("koa-router")();
+const article = require("./article");
+const meeting = require("./meeting");
 const routers = router
-	// Article
-	.get('/articles', article.list)
-	.get('/articles/:id', article.get)
-	.post('/articles', article.create)
-	.delete('/articles/delete/:id', article.delete)
+
+  // Article
+  .get('/articles', article.list)
+  .get('/articles/:id', article.get)
+  .post('/articles', article.create)
+  .delete('/articles/delete/:id', article.delete)
+
+  //Meeting
+  .get("/meetings", meeting.list)
+  .post("/meetings", meeting.create)
+  .post("/meetings/enroll/:_id", meeting.enroll)
+  .delete("/meetings/:_id", meeting.delete)
+  .patch("/meetings/:_id", meeting.edit)
+  .get("/meetings/findByName/:name", meeting.searchByName)
+  .get("/meetings/getlist/:userID", meeting.getList)
+  .get("/meetings/getpostlist/:userID", meeting.getPostList);
 // 基本款CRUD的命名：create, delete,update, list拿所有/get根据id拿单个
 module.exports = routers;
