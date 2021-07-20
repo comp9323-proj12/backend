473a071709147f507c9adb5ffb6f09ad5dc1cf20
adam <wangzhao960917@gmail.com>
2021-07-20 20:20:17 +1000

    CRUD for meeting first version

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ const routers = router
 
   // Article
   .get("/articles", article.list)
-  .post("/articles", article.create);
+  .post("/articles", article.create)
 
-//Meeting
+  //Meeting
+  .get("/meetings", meeting.list)
+  .post("/meetings", meeting.create)
+  .post("/meetings/enroll/:_id", meeting.enroll)
+  .delete("/meetings/:_id", meeting.delete)
+  .patch("/meetings/:_id", meeting.edit)
+  .get("/meetings/findByName/:name", meeting.searchByName)
+  .get("/meetings/getlist/:userID", meeting.getList)
+  .get("/meetings/getpostlist/:userID", meeting.getPostList);
 // 基本款CRUD的命名：create, delete,update, list拿所有/get根据id拿单个
 module.exports = routers;
