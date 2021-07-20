2951d301b74e054f1993a45b296f4159f1319d2e
adam <wangzhao960917@gmail.com>
2021-07-20 20:18:40 +1000

    meeting model

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-const router = require('koa-router')();
-const article = require('./article');
-
+const router = require("koa-router")();
+const article = require("./article");
+const meeting = require("./meeting");
 const routers = router
-	// Article
-	.get('/articles', article.list)
-	.post('/articles', article.create)
 
+  // Article
+  .get("/articles", article.list)
+  .post("/articles", article.create);
+
+//Meeting
 // 基本款CRUD的命名：create, delete,update, list拿所有/get根据id拿单个
 module.exports = routers;
