73af22c4984bcf19a31248be46160aabe999ca79
adam <wangzhao960917@gmail.com>
2021-07-20 20:16:01 +1000

    modify meeting model

--- models/Meeting.js
+++ models/Meeting.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const MeetingSchema = new Schema({
-    hosts: {
-        type: [{type: Schema.Types.ObjectId, ref: 'User'}],
-		required: true,
-    },
-    title: {
-        type: String,
-		required: true
-    },
-    link: {
-        type: String,
-		required: true
-    },
-    notice: {  //发布类似简短的公告
-        type: String,
-		required: true
-    },
-    startTime: {
-        type: Date,
-		required: true
-    },
-    creatTime: {
-        type: Date,
-		default: Date.now
-    }
+  instructor: {
+    //主讲人
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  notice: {
+    //发布类似简短的公告
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: String, //数据类型改变
+    required: true,
+  },
+  creatTime: {
+    type: Date,
+    default: Date.now,
+  },
+  students: {
+    //参与会议人员
+    type: [String],
+    default: [],
+  },
 });
-module.exports = mongoose.model('Meeting', MeetingSchema);
+module.exports = mongoose.model("Meeting", MeetingSchema);
