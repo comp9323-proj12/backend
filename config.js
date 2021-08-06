9b47f2a171277e3dc85b308e0d43028c8328a8bc
feifeirun <feifeirun97@gmail.com>
2021-08-07 01:36:41 +1000

    Deploy

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'database': 'mongodb://localhost:27017/comp'
+	'databaseLocal': 'mongodb://localhost:27017/comp',
+	'database':'mongodb+srv://user0:lFQklASVLAgBvRTk@cluster0.szcon.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
